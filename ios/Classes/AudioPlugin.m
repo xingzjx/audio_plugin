80dfa8f40e789e4f03121e93fcff0a62aa8397f2
xingzjx <1181313247@qq.com>
2019-03-28 16:56:24 +0800

    feat:init

---
+++ ios/Classes/AudioPlugin.m
@@ -0,0 +1,20 @@
+#import "AudioPlugin.h"
+
+@implementation AudioPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"audio_plugin"
+            binaryMessenger:[registrar messenger]];
+  AudioPlugin* instance = [[AudioPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
